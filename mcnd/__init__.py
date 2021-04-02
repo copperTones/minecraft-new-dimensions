3d81f54af37408b9826e1beed1a6192aa2b2d03f
amp8wy <amp8wy@mail.umsl.edu>
2021-04-02 14:22:30 -0500

    it makes zips

--- mcnd/__init__.py
+++ mcnd/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, Response
 from json import dumps
+from io import BytesIO
+from zipfile import ZipFile
 
 
 def create_app(test_config=None):
@@ -9,9 +11,13 @@ def create_app(test_config=None):
     def test():
         return render_template('download.html')
     
-    @app.route('/download.json')
+    @app.route('/download')
     def download():
-        return Response(dumps({'test':1}),
+        mem_io = BytesIO()
+        with ZipFile(mem_io, 'w') as zf:
+            zf.writestr('test.json', dumps({"test": 2}))
+        mem_io.seek(0)
+        return Response(mem_io,
                 headers={"Content-Disposition":
                 "attachment;filename=test.json"})
 
