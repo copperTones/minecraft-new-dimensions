5d7a5ae067812ff09467c4b4cb03afd5234e7ada
amp8wy <amp8wy@mail.umsl.edu>
2021-04-03 18:26:26 -0500

    more figuring stuff out

--- mcnd/__init__.py
+++ mcnd/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from json import dumps
+import os
 from io import BytesIO
 from zipfile import ZipFile
 
@@ -7,18 +8,18 @@ from zipfile import ZipFile
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    @app.route('/test')
+    @app.route('/test', methods=('GET', 'POST'))
     def test():
-        return render_template('download.html')
-    
-    @app.route('/download')
-    def download():
+        if request.method == 'GET':
+            return render_template('download.html')
+
         mem_io = BytesIO()
         with ZipFile(mem_io, 'w') as zf:
-            zf.writestr('test.json', dumps({"test": 2}))
+            zf.writestr(os.path.join('New Dimensions', 'pack.mcmeta'),
+                    dumps({"pack": {"description": request.form['description'], "pack_format": 6}}))
         mem_io.seek(0)
         return Response(mem_io,
                 headers={"Content-Disposition":
-                "attachment;filename=test.json"})
-
+                "attachment;filename=new_dimensions.zip"})
+    
     return app
